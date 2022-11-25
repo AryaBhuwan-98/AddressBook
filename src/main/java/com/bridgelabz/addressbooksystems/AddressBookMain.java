ffa2afa2f9a3c04fca18993a526e5239186defbd
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-11-24 20:42:29 -0800

    Add: adding program for Add Directory

--- src/main/java/com/bridgelabz/addressbooksystems/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbooksystems/AddressBookMain.java
@@ -3,12 +3,15 @@ package com.bridgelabz.addressbooksystems;
 import java.util.Scanner;
 
 public class AddressBookMain {
-	static Scanner scannerObject = new Scanner(System.in);
-
 	public static void main(String[] args) {
 		System.out.println("---------- Welcome To Address Book Program!! ----------");
-		AdressBook addressBook = new AdressBook();
+		
+		AddressBookMain addressBookDirectory = new AddressBookMain();
+        addressBookDirectory.operationDirectory();
+        Address addressBook = new Address();
         addressBook.operation();
 	}
+	private void operationDirectory() {
+		// TODO Auto-generated method stub
+	}
 }
-
